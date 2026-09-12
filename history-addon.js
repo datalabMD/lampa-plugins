9904be2eb44bdc33d5727b7667f206ede31e2d51
datalabMD <91216162+datalabMD@users.noreply.github.com>
2026-09-12 17:26:48 +0300

    Load translator pin fix after native playlist layer

--- history-addon.js
+++ history-addon.js
@@ -5,5 +5,5 @@ if(window.rezka4_dynamic_feature_loader)return;window.rezka4_dynamic_feature_loa
 var stamp=Date.now();
 function loadOne(urls,done){var i=0;function next(){if(i>=urls.length)return done&&done(false);var s=document.createElement('script');s.async=false;s.src=urls[i++]+(urls[i-1].indexOf('?')>=0?'&':'?')+'ts='+stamp;s.onload=function(){done&&done(true)};s.onerror=function(){try{s.remove()}catch(e){}next()};(document.head||document.documentElement).appendChild(s)}next()}
 function github(path){return['https://cdn.jsdelivr.net/gh/datalabMD/lampa-plugins@main/'+path,'https://raw.githubusercontent.com/datalabMD/lampa-plugins/main/'+path]}
-loadOne(github('history-core.js'),function(ok){if(!ok){try{console.log('REZKA4 history core unavailable')}catch(e){}return}loadOne(github('player-playlist-addon.js'),function(playlistOk){if(!playlistOk)try{console.log('REZKA4 player playlist layer unavailable')}catch(e){}})});
+loadOne(github('history-core.js'),function(ok){if(!ok){try{console.log('REZKA4 history core unavailable')}catch(e){}return}loadOne(github('player-playlist-addon.js'),function(playlistOk){if(!playlistOk){try{console.log('REZKA4 player playlist layer unavailable')}catch(e){}return}loadOne(github('player-translator-fix.js'),function(fixOk){if(!fixOk)try{console.log('REZKA4 translator pin fix unavailable')}catch(e){}})})});
 })();
